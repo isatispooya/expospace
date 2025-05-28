587761d0984e3ff4c2bee0f93f663ba1ba498f45
moeen dehqan <moeen.dehqan@gmail.com>
2025-05-28 14:12:11 +0330

    init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,32 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { WebView } from 'react-native-webview';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  const injectedJS = `
+  document.addEventListener('contextmenu', event => event.preventDefault());
+  document.addEventListener('keydown', function (event) {
+    if (
+      event.key === 'F12' ||
+      (event.ctrlKey && event.shiftKey && event.key === 'I') ||
+      (event.ctrlKey && event.shiftKey && event.key === 'J') ||
+      (event.ctrlKey && event.key === 'U')
+    ) {
+      event.preventDefault();
+    }
   });
+  true;
+`;
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+
+
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <WebView
+    javaScriptEnabled={true}
+    domStorageEnabled={true}
+    injectedJavaScript={injectedJS}
+    source={{ uri: 'https://my.isatispooya.com' }}
+    />
   );
 }
